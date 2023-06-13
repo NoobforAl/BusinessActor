3f06ec857134b286f9629d946f2d1cc1d6e10f4c
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-06-13 12:15:54 +0330

    add loder env file and env values
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

---
+++ src/loadEnv/loadEnv.go
@@ -0,0 +1,49 @@
+package env
+
+import (
+	"errors"
+	"fmt"
+	"net/netip"
+	"os"
+	"strconv"
+
+	"github.com/NoobforAl/BusinessActor/logger"
+	"github.com/joho/godotenv"
+)
+
+var (
+	dsn         string
+	listen_IP   string
+	listen_PORT int
+)
+
+var errNotValidIP = errors.New("ip4 is not valid, ")
+var errNotPort = errors.New("Port is not valid, ")
+
+func init() {
+	if err := godotenv.Load("./.env"); err != nil {
+		logger.Log.Println("No .env file found")
+	}
+
+	dsn = os.Getenv("DSN")
+	listen_IP = os.Getenv("LISTEN_IP")
+
+	ip, err := netip.ParseAddr(listen_IP)
+	if err != nil && !ip.Is4() {
+		panic(errors.Join(errNotValidIP, err))
+	}
+
+	port := os.Getenv("LISTEN_PORT")
+	listen_PORT, err = strconv.Atoi(port)
+	if err != nil || listen_PORT < 1 {
+		panic(errors.Join(errNotPort, err))
+	}
+}
+
+func GetDsn() string {
+	return dsn
+}
+
+func GetAddrListen() string {
+	return fmt.Sprintf("%s:%d", listen_IP, listen_PORT)
+}
