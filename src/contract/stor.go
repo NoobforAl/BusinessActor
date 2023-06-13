fa48158b410eb3aec19fd3ba35cda577b2d7bf7f
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-06-13 17:30:50 +0330

    add contract Stor interface
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

---
+++ src/contract/stor.go
@@ -0,0 +1,25 @@
+package contract
+
+import (
+	"context"
+
+	"github.com/NoobforAl/BusinessActor/src/db"
+)
+
+type Stor interface {
+	NewBA() *db.BusinessActor
+
+	CountBusinessActor(c context.Context, filter any) (int64, error)
+
+	GetOneBusinessActor(c context.Context, filter any) (db.BusinessActor, error)
+	GetManyBusinessActor(c context.Context, filter any) ([]db.BusinessActor, error)
+
+	InsertBusinessActor(c context.Context, d any) error
+	InsertManyBusinessActor(c context.Context, d []db.BusinessActor) error
+
+	UpdateBusinessActor(c context.Context, filter any, update any) error
+	UpdateManyBusinessActor(c context.Context, filter any, update any) error
+
+	DeleteBusinessActor(c context.Context, filter any) error
+	DeleteManyBusinessActor(c context.Context, filter any) error
+}
