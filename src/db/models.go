24a051cc8701e975cc6e1b85d476c4d8f0463d04
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-06-13 17:28:51 +0330

    add business actor model
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

---
+++ src/db/models.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BusinessActor struct {
+	ID primitive.ObjectID `bson:"_id"`
+
+	Series_reference string `bson:"series_reference"`
+
+	Period     time.Time `bson:"period"`
+	Data_value float64   `bson:"data_value"`
+
+	Suppressed bool `bson:"suppressed"`
+
+	STATUS    string `bson:"status"`
+	UNITS     string `bson:"units"`
+	Magnitude int    `bson:"magnitude"`
+
+	Subject string `bson:"subject"`
+	Group   string `bson:"group"`
+
+	Series_title_1 string `bson:"series_title_1"`
+	Series_title_2 string `bson:"series_title_2"`
+	Series_title_3 string `bson:"series_title_3"`
+	Series_title_4 string `bson:"series_title_4"`
+	Series_title_5 string `bson:"series_title_5"`
+}
