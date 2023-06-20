97c6ccfd35775e40ec8a82cc5c77d6e1b0fe6aac
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-06-21 01:14:23 +0330

    fix controller for new change
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/controller/businessActor.go
+++ src/controller/businessActor.go
@@ -4,16 +4,19 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/NoobforAl/BusinessActor/src/action"
 	"github.com/NoobforAl/BusinessActor/src/contract"
-	"github.com/NoobforAl/BusinessActor/src/entity"
 	"github.com/gin-gonic/gin"
+
+	. "github.com/NoobforAl/BusinessActor/src/errors"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func FindBusinessActor(stor contract.Stor) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id := c.Param("id")
-		ba := entity.BusinessActor{}
-		if err := ba.Find(stor, c, id); err != nil {
+		ba, err := action.NewBaActor(stor).Find(c, id)
+		if err != nil {
 			handleErr(c, err)
 			return
 		}
@@ -35,28 +38,27 @@ func GetManyBusinessActor(stor contract.Stor) gin.HandlerFunc {
 			return
 		}
 
-		ba := entity.BusinessActor{}
-
-		businessActors, err := ba.GetMany(stor, c, page, size)
+		data, err := action.NewBaActor(stor).GetMany(c, page, size)
 		if err != nil {
-			handleErr(c, errors.Join(ErrBadRequest, err))
+			handleErr(c, err)
 			return
 		}
-		c.JSON(http.StatusOK, businessActors)
+		c.JSON(http.StatusOK, data)
 	}
 }
 
 func CreateBusinessActor(stor contract.Stor) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ba := entity.BusinessActor{}
+		ac := action.NewBaActor(stor)
 		var err error
 
-		if err = ba.Pars(c); err != nil {
+		ba, err := ac.Pars(c)
+		if err != nil {
 			handleErr(c, errors.Join(ErrBadRequest, err))
 			return
 		}
 
-		if err = ba.Create(stor, c); err != nil {
+		if err = ac.Create(c, ba); err != nil {
 			handleErr(c, err)
 			return
 		}
@@ -70,16 +72,17 @@ func CreateBusinessActor(stor contract.Stor) gin.HandlerFunc {
 // in database changed value to empty !!
 func UpdateBusinessActor(stor contract.Stor) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ba := entity.BusinessActor{}
+		ac := action.NewBaActor(stor)
 		id := c.Param("id")
 		var err error
 
-		if err = ba.Pars(c); err != nil {
+		ba, err := ac.Pars(c)
+		if err != nil {
 			handleErr(c, errors.Join(ErrBadRequest, err))
 			return
 		}
 
-		if err = ba.Update(stor, c, id); err != nil {
+		if err = ac.Update(c, ba, id); err != nil {
 			handleErr(c, err)
 			return
 		}
@@ -90,14 +93,28 @@ func UpdateBusinessActor(stor contract.Stor) gin.HandlerFunc {
 
 func DeleteBusinessActor(stor contract.Stor) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ba := entity.BusinessActor{}
+		ac := action.NewBaActor(stor)
 		id := c.Param("id")
 
-		if err := ba.Delete(stor, c, id); err != nil {
+		if err := ac.Delete(c, id); err != nil {
 			handleErr(c, err)
 			return
 		}
 
-		c.JSON(http.StatusOK, ba)
+		c.JSON(http.StatusOK, gin.H{})
 	}
+}
+
+func handleErr(c *gin.Context, err error) {
+	status := http.StatusInternalServerError
+	switch {
+	case errors.Is(err, primitive.ErrInvalidHex) ||
+		errors.Is(err, ErrBadRequest):
+		status = http.StatusBadRequest
+
+	case errors.Is(err, ErrNotFoundPage) ||
+		errors.Is(err, ErrNotFound):
+		status = http.StatusNotFound
+	}
+	c.JSON(status, gin.H{"detail": err.Error()})
 }
