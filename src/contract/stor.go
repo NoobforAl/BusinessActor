77f4573c54c983963f4272f9fd62293dad2aabf4
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-06-21 01:10:24 +0330

    fix business actor for new change
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/contract/stor.go
+++ src/contract/stor.go
@@ -3,24 +3,55 @@ package contract
 import (
 	"context"
 
-	"github.com/NoobforAl/BusinessActor/src/db"
+	"github.com/NoobforAl/BusinessActor/src/entity"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type BusinessActor interface {
+	CountBusinessActor(
+		c context.Context, filter any,
+	) (int64, error)
+
+	GetOneBusinessActor(
+		c context.Context, filter any,
+	) (entity.BusinessActor, error)
+
+	GetManyBusinessActor(
+		c context.Context,
+		filter any,
+		opts ...*options.FindOptions,
+	) ([]entity.BusinessActor, error)
+
+	InsertBusinessActor(
+		c context.Context, d entity.BusinessActor,
+	) error
+
+	InsertManyBusinessActor(
+		c context.Context,
+		d []entity.BusinessActor,
+	) error
+
+	UpdateBusinessActor(
+		c context.Context,
+		filter any,
+		update entity.BusinessActor,
+	) error
+
+	UpdateManyBusinessActor(
+		c context.Context,
+		filter any,
+		update entity.BusinessActor,
+	) error
+
+	DeleteBusinessActor(
+		c context.Context, filter any,
+	) error
+
+	DeleteManyBusinessActor(
+		c context.Context, filter any,
+	) error
+}
+
 type Stor interface {
-	NewBA() *db.BusinessActor
-
-	CountBusinessActor(c context.Context, filter any) (int64, error)
-
-	GetOneBusinessActor(c context.Context, filter any) (db.BusinessActor, error)
-	GetManyBusinessActor(c context.Context, filter any, opts ...*options.FindOptions) ([]db.BusinessActor, error)
-
-	InsertBusinessActor(c context.Context, d any) error
-	InsertManyBusinessActor(c context.Context, d []db.BusinessActor) error
-
-	UpdateBusinessActor(c context.Context, filter any, update any) error
-	UpdateManyBusinessActor(c context.Context, filter any, update any) error
-
-	DeleteBusinessActor(c context.Context, filter any) error
-	DeleteManyBusinessActor(c context.Context, filter any) error
+	BusinessActor
 }
