81d068823dd7c7215a0f9e7d0833bb25eed508de
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-06-21 01:14:55 +0330

    match main with router api
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"net/http"
 
 	loadCsv "github.com/NoobforAl/BusinessActor/src/businessActorCsv"
-	"github.com/NoobforAl/BusinessActor/src/controller"
 	"github.com/NoobforAl/BusinessActor/src/db"
 	env "github.com/NoobforAl/BusinessActor/src/loadEnv"
 	"github.com/NoobforAl/BusinessActor/src/logger"
+	"github.com/NoobforAl/BusinessActor/src/router"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,16 +17,7 @@ func main() {
 	loadCsv.InitData(stor, env.GetCsvPath())
 
 	api := r.Group("/api")
-	{
-		api.GET("/get/:id", controller.FindBusinessActor(stor))
-		api.GET("/getMany", controller.GetManyBusinessActor(stor))
-
-		api.POST("/create", controller.CreateBusinessActor(stor))
-
-		api.PUT("/update/:id", controller.UpdateBusinessActor(stor))
-
-		api.DELETE("/delete/:id", controller.DeleteBusinessActor(stor))
-	}
+	router.BaRouter(api, stor)
 
 	if err := http.ListenAndServe(env.GetAddrListen(), r); err != nil {
 		logger.Log.Fatal(err)
